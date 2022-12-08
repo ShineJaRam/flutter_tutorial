cfcffc22a1c4df1e4d3c58c67ef832189c0fe177
Roy <roy@madsq.net>
2022-12-08 19:01:16 +0900

    221208-1

---
+++ future_and_stream_builder/lib/screen/home_screen.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontSize: 16,
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: getNumber(),
+          builder: (context, snapshot) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Future Builder',
+                  style: textStyle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  'ConState : ${snapshot.connectionState}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Data : ${snapshot.data}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Error : ${snapshot.error}',
+                  style: textStyle,
+                ),
+                ElevatedButton(onPressed: () {
+                  setState(() {
+
+                  });
+                }, child: Text('Set State',style: textStyle,),),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    final random = Random();
+
+    return random.nextInt(100);
+  }
+}
